e9d7a097df5b10fda09b61357dbb7863e8c63c40
Ootzk <ootzk@kakao.com>
2025-03-08 01:10:26 +0900

    🍱 Support CLIEngine official assets

---
+++ include/CLIEngine/assets/logo.hpp
@@ -0,0 +1,150 @@
+#include "CLIEngine/core.hpp"
+
+namespace CLIEngine {
+
+namespace Assets {
+
+const CLIEngine::Sprite sprite_logo{
+    {
+        "_____ _      _____ ______             _             ",
+        "/ ____| |    |_   _|  ____|           (_)           ",
+        "| |   | |      | | | |__   _ __   __ _ _ _ __   ___ ",
+        "| |   | |      | | |  __| | '_ \\ / _` | | '_ \\ / _ \\",
+        "| |___| |____ _| |_| |____| | | | (_| | | | | |  __/",
+        "\\_____|______|_____|______|_| |_|\\__, |_|_| |_|\\___|",
+        "                                  __/ |             ",
+        "                                 |___/              "
+    },
+    {
+        "RRRRRRBBBBBBBGGGGGGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "RRRRRRBBBBBBBGGGGGGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "RRRRRRBBBBBBBGGGGGGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "RRRRRRBBBBBBBGGGGGGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "RRRRRRBBBBBBBGGGGGGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "RRRRRRBBBBBBBGGGGGGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "RRRRRRBBBBBBBGGGGGGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "RRRRRRBBBBBBBGGGGGGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    },
+    {
+        "                   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        "                   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        "                   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        "                   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        "                   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        "                   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        "                   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        "                   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    }
+};
+
+const CLIEngine::Sprite sprite_logo_C{
+    {
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    "
+    },
+    {
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    "
+    },
+    {
+        "RRRRRRRRRRRRRRRRRRRR",
+        "RR                RR",
+        "R    RRRRRRRRRRR   R",
+        "R   RRRRRRRRRRRRRRRR",
+        "R   RRRRRRRRRRRRRRRR",
+        "R   RRRRRRRRRRRRRRRR",
+        "R    RRRRRRRRRRR   R",
+        "RR                RR",
+        "RRRRRRRRRRRRRRRRRRRR",
+    }
+};
+
+const CLIEngine::Sprite sprite_logo_L{
+    {
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    "
+    },
+    {
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    "
+    },
+    {
+        "BBBBBBBBBBBBBBBBBBBB",
+        "BB   BBBBBBBBBBBBBBB",
+        "BB   BBBBBBBBBBBBBBB",
+        "BB   BBBBBBBBBBBBBBB",
+        "BB   BBBBBBBBBBBBBBB",
+        "BB   BBBBBBBBBBBBBBB",
+        "BB   BBBBBBBBBBBBBBB",
+        "BB                BB",
+        "BBBBBBBBBBBBBBBBBBBB",
+    }
+};
+
+const CLIEngine::Sprite sprite_logo_I{
+    {
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    "
+    },
+    {
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    "
+    },
+    {
+        "GGGGGGGGGGGGGGGGGGGG",
+        "GGG              GGG",
+        "GGGGGGGGG   GGGGGGGG",
+        "GGGGGGGGG   GGGGGGGG",
+        "GGGGGGGGG   GGGGGGGG",
+        "GGGGGGGGG   GGGGGGGG",
+        "GGGGGGGGG   GGGGGGGG",
+        "GGG              GGG",
+        "GGGGGGGGGGGGGGGGGGGG",
+    }
+};
+
+}
+
+}
