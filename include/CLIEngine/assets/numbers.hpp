aaf922edcdc9a57cf0d7ba3acca5b97eeaf53c49
Ootzk <ootzk@kakao.com>
2025-03-08 06:16:29 +0900

    🍱 Add number assets

---
+++ include/CLIEngine/assets/numbers.hpp
@@ -0,0 +1,242 @@
+#include "CLIEngine/core.hpp"
+
+namespace CLIEngine {
+
+namespace Assets {
+
+const std::vector<CLIEngine::Sprite> numbers_8x5_font1{
+    { // 0
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "WWXXXXWW",
+            "WXXWWXXW",
+            "WXXWWXXW",
+            "WXXWWXXW",
+            "WWXXXXWW"
+        }
+    },
+    {  // 1
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "WXXXXWWW",
+            "WWWXXWWW",
+            "WWWXXWWW",
+            "WWWXXWWW",
+            "WXXXXXXW"
+        }
+    },
+    {  // 2
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "WWXXXXWW",
+            "WXXWWXXW",
+            "WWWWXXWW",
+            "WWXXWWWW",
+            "WXXXXXXW"
+        }
+    },
+    {  // 3
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "WWXXXXWW",
+            "WXXWWXXW",
+            "WWWWXXXW",
+            "WXXWWXXW",
+            "WWXXXXWW"
+        }
+    },
+    {  // 4
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "WXXWWXXW",
+            "WXXWWXXW",
+            "WXXXXXXW",
+            "WWWWWXXW",
+            "WWWWWXXW"
+        }
+    },
+    {  // 5
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "WXXXXXXW",
+            "WXXWWWWW",
+            "WXXXXXWW",
+            "WWWWWXXW",
+            "WXXXXXWW"
+        }
+    },
+    {  // 6
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "WWXXXXWW",
+            "WXXWWWWW",
+            "WXXXXXWW",
+            "WXXWWXXW",
+            "WWXXXXWW"
+        }
+    },
+    {  // 7
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "WXXXXXXW",
+            "WWWWXXWW",
+            "WWWXXWWW",
+            "WWXXWWWW",
+            "WXXWWWWW"
+        }
+    },
+    {  // 8
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "WWXXXXWW",
+            "WXXWWXXW",
+            "WWXXXXWW",
+            "WXXWWXXW",
+            "WWXXXXWW"
+        }
+    },
+    {  // 9
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "        "
+        },
+        {
+            "WWXXXXWW",
+            "WXXWWXXW",
+            "WWXXXXXW",
+            "WWWWWXXW",
+            "WWXXXXWW"
+        }
+    }
+};
+
+}
+
+}
